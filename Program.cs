e6e3ca02c5f2a0a31d46f15ebfd351b1d4944c99
Aspector1 <84536195+Aspector1@users.noreply.github.com>
2022-11-06 19:37:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace PageIn
+{
+    class Program
+    {
+        [DllImport("kernel32.dll")]
+        static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int dwSize, out IntPtr lpNumberOfBytesRead);
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: Unpager <PID>");
+                return;
+            }
+
+            int pid = int.Parse(args[0]);
+            Process process = Process.GetProcessById(pid);
+            IntPtr hProcess = process.Handle;
+
+            foreach (ProcessModule module in process.Modules)
+            {
+                IntPtr baseAddress = module.BaseAddress;
+                int size = module.ModuleMemorySize;
+                byte[] buffer = new byte[size];
+                IntPtr bytesRead;
+
+                if (ReadProcessMemory(hProcess, baseAddress, buffer, size, out bytesRead))
+                {
+                    Console.WriteLine("Read {0} bytes from {1}", bytesRead, module.ModuleName);
+                }
+                else
+                {
+                    Console.WriteLine("Failed to read from {0}", module.ModuleName);
+                }
+            }
+        }
+    }
+}
