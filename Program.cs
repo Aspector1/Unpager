5ddfc5ee68c7e872b3c7c149e87057efe89daced
Aspector1 <84536195+Aspector1@users.noreply.github.com>
2022-11-06 22:16:38 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,31 +15,47 @@ namespace Unpager
 
         static void Main(string[] args)
         {
-            if (args.Length != 1)
+            if (args.Length != 2)
             {
-                Console.WriteLine("Usage: Unpager <PID>");
+                Console.WriteLine("Usage: Unpager <PID or Process Name> <interval>");
                 return;
             }
 
-            int pid = int.Parse(args[0]);
+            int pid;
+            if (!int.TryParse(args[0], out pid))
+            {
+                Process[] processes = Process.GetProcessesByName(args[0]);
+                if (processes.Length == 0)
+                {
+                    Console.WriteLine("Process not found");
+                    return;
+                }
+                pid = processes[0].Id;
+            }
+            // int pid = int.Parse(args[0]);
+            int interval = int.Parse(args[1]);
             Process process = Process.GetProcessById(pid);
             IntPtr hProcess = process.Handle;
 
-            foreach (ProcessModule module in process.Modules)
+            while (true)
             {
-                IntPtr baseAddress = module.BaseAddress;
-                int size = module.ModuleMemorySize;
-                byte[] buffer = new byte[size];
-                IntPtr bytesRead;
+                foreach (ProcessModule module in process.Modules)
+                {
+                    IntPtr baseAddress = module.BaseAddress;
+                    int size = module.ModuleMemorySize;
+                    byte[] buffer = new byte[size];
+                    IntPtr bytesRead;
 
-                if (ReadProcessMemory(hProcess, baseAddress, buffer, size, out bytesRead))
-                {
-                    Console.WriteLine("Read {0} bytes from {1}", bytesRead, module.ModuleName);
+                    if (ReadProcessMemory(hProcess, baseAddress, buffer, size, out bytesRead))
+                    {
+                        Console.WriteLine("Read {0} bytes from {1}", bytesRead, module.ModuleName);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed to read from {0}", module.ModuleName);
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("Failed to read from {0}", module.ModuleName);
-                }
+                System.Threading.Thread.Sleep(interval * 1000);
             }
         }
     }
